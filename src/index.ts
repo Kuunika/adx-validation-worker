3ff9778671f57b05f06a870b98e3e0938c2de1fd
ibwighane <bsmwalwanda@gmail.com>
2019-04-16 10:57:15 +0200

    removed the need for else clauses

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as  amqp from 'amqplib/callback_api';
 import { Channel, Connection, Message } from 'amqplib/callback_api';
 import { config } from 'dotenv'; config(); //initialize right there the configuration
-import { readFileSync } from 'fs';
 import { QueueMessage } from './types';
 import { PayloadSchema } from './schemas';
 import { connectToDatabase } from './datasource';
@@ -15,13 +14,17 @@ import {
 }
   from './modules';
 
-amqp.connect(process.env.AVW_QUEUE_HOST || 'amqp://localhost', function (error: Error, connection: Connection) {
-  connection.createChannel(function (error: Error, channel: Channel) {
-    var queueName = process.env.AVW_QUEUE_NAME || "DHIS2_VALIDATION_QUEUE";
-    channel.assertQueue(queueName, { durable: true });
-    console.log("[*] Waiting for messages on %s. To exit press CTRL+C", queueName);
-    channel.consume(queueName, async function (message: Message | null) {
-      if (message) {
+const main = () => {
+  const host = process.env.AVW_QUEUE_HOST || 'amqp://localhost';
+  amqp.connect(host, function (error: Error, connection: Connection) {
+    connection.createChannel(function (error: Error, channel: Channel) {
+      var queueName = process.env.AVW_QUEUE_NAME || "DHIS2_VALIDATION_QUEUE";
+      channel.assertQueue(queueName, { durable: true });
+      console.log("[*] Waiting for messages on %s. To exit press CTRL+C", queueName);
+      channel.consume(queueName, async function (message: Message | null) {
+        if (!message) {
+          return
+        }
         const queueMessage: QueueMessage = JSON.parse(message.content.toString());
         const logger = new Logger(queueMessage.channelId);
         const sequelize = await connectToDatabase(
@@ -43,16 +46,19 @@ amqp.connect(process.env.AVW_QUEUE_HOST || 'amqp://localhost', function (error: 
         const payload = JSON.parse(payloadFileContent);
         const { error } = Joi.validate(payload, PayloadSchema);
 
-        if (!error) {
-          await recordStructureValidationStatus(sequelize, migration.get('id'), true);
-          logger.info('Payload passed structure validation');
-          //TODO: start off on the other validation - authorization
-        } else {
+        if (error) {
           await recordStructureValidationStatus(sequelize, migration.get('id'), false);
           logger.info('Payload failed structure validation, sending email');
+          return
           //TODO: Might wanna send an email at this point
         }
-      }
-    }, { noAck: false });
+        await recordStructureValidationStatus(sequelize, migration.get('id'), true);
+        logger.info('Payload passed structure validation');
+        //TODO: start off on the other validation - authorization
+
+      }, { noAck: false });
+    });
   });
-});
+}
+
+main();
