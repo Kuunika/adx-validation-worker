d39394e2b8398c4de6b310ada76d18ddf8e4d019
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-05-05 08:20:30 +0200

    test(Wrappers): Write spec for env wrapper

---
+++ test/unit/utils/envwrapper.spec.ts
@@ -0,0 +1,21 @@
+import { describe } from "mocha";
+import { Wrapper } from "../../../src/utils/Environment";
+import { expect } from "chai";
+
+describe("#Environment wrapper test", () => {
+    const subject: Wrapper = Wrapper;
+
+    it("~should be able to get a value/null given a key", () => {
+        expect(Wrapper.get('randomkey')).to.equal(null);
+    });
+
+    it("~should be able to set a value given a key", () => {
+        Wrapper.set('randomkey', 'daniel');
+        expect(Wrapper.get('randomkey')).to.equal('daniel');
+    });
+
+    it("~should be able to delete a value given a key", () => {
+        Wrapper.delete('randomkey');
+        expect(Wrapper.get('randomkey')).to.equal(null);
+    });
+});
