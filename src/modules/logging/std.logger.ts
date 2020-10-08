84c443911796b48114382e8c2a856c2534442084
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-10-08 16:08:46 +0200

    refactor(Logging): add standard logger

---
+++ src/modules/logging/std.logger.ts
@@ -0,0 +1,15 @@
+export class StdLogger {
+    constructor(private readonly namespace: string) {}
+
+    log(message: string): void {
+        console.log(`${this.namespace}: ${message}`);
+    }
+
+    error(message: string): void {
+        console.error(`${this.namespace}: ${message}`);
+    }
+
+    debug(message: string): void {
+        console.debug(`${this.namespace}: ${message}`);
+    }
+}
