97cf7bced3c19f9aad9589bc4312f7c88eee9161
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-30 09:17:29 +0200

    refactor(MHFRClient): migrate logger

--- src/modules/facilities/master-health-facility.client.ts
+++ src/modules/facilities/master-health-facility.client.ts
@@ -1,26 +1,24 @@
 import Axios, { AxiosError } from 'axios';
 import { Wrapper } from "../config/wrapper";
-import { StdLogger } from "../logging/std.logger";
+import { LOGGER } from '../logging/winston.logger';
 import { Facility } from "./facility.interface";
 import { MasterHealthFacilityClientGatewayException } from './master-health-facility-client.exception';
 
 export class MasterHealthFacilityClient {
-    private logger: StdLogger = new StdLogger('MHFR Client');
-
     async findFacilityByCode(code: string): Promise<Facility|null> {
         const host = Wrapper.get<string>('AVW_MHFR_URL');
-        const facilityPath = `${host}/Facilities/findByCode/${code}`;
+        const facilityPath = `${host}/Facilities/${code}`;
         try {
-            this.logger.debug(`Attempting to find facility ${facilityPath}.`)
+            LOGGER.info(`Attempting to find facility ${facilityPath}.`);
             const facility = (await Axios.get(facilityPath)).data;
-            this.logger.debug(`Found facility ${facilityPath}`);
+            LOGGER.info(`Found facility ${facilityPath}`);
             return facility;
         } catch(error) {
             const err = error as AxiosError;
             if (err && err.response && err.response.status === 404) {
-                this.logger.error(`Failed to find facility ${facilityPath}. ${err.response.statusText}.`);
+                LOGGER.error(`Failed to find facility ${facilityPath}. ${err.response.statusText}.`);
             } else {
-                this.logger.error(`Failed to find facility ${facilityPath}. Gateway error.`);
+                LOGGER.error(`Failed to find facility ${facilityPath}. Gateway error.`);
             }
             return null;
             // throw new MasterHealthFacilityClientGatewayException(`There was a problem talking to MHFR when fetching ${code}.`);
