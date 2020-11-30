8a7ea0d13efd2cb1b68a11037ec7cee5c777d697
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-30 08:28:53 +0200

    feat(Logging): add stdout logger

--- src/modules/logging/std.logger.ts
+++ src/modules/logging/std.logger.ts
@@ -2,14 +2,14 @@ export class StdLogger {
     constructor(private readonly namespace: string) {}
 
     log(message: string): void {
-        console.log(`${this.namespace}: ${message}`);
+        console.log(`[${this.namespace}]: ${message}`);
     }
 
     error(message: string): void {
-        console.error(`${this.namespace}: ${message}`);
+        console.error(`[${this.namespace}]: ${message}`);
     }
 
     debug(message: string): void {
-        console.debug(`${this.namespace}: ${message}`);
+        console.debug(`[${this.namespace}]: ${message}`);
     }
 }
