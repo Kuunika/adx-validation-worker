be3050edeb36fedf3c5a4dd9c14cf54727d05d12
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-30 08:28:25 +0200

    refactor(Validator): add more logging

--- src/modules/validationOperations.ts
+++ src/modules/validationOperations.ts
@@ -1,3 +1,5 @@
+import { Sequelize } from "sequelize";
+import { appendFileSync } from "fs";
 import {
   PostPayload,
   MigrationDataElement,
@@ -6,16 +8,16 @@ import {
   QueueMessage
 } from "../interfaces";
 import { persistValidationFailures } from ".";
-import { Sequelize } from "sequelize";
-import { appendFileSync } from "fs";
 import { sendToLogQueue } from "./queueOperations";
 import { ProductMasterClient } from "./products/product-master.client";
 import { MasterHealthFacilityClient } from "./facilities/master-health-facility.client";
 import { getDHIS2OUCode } from "./facilities/helpers";
 import { getProductDHIS2Code } from "./products/helpers";
+import { StdLogger } from "./logging/std.logger";
 
 const productMasterClient = new ProductMasterClient();
 const mhfrClient = new MasterHealthFacilityClient();
+const logger = new StdLogger('Validation Operations');
 
 export async function createMappedPayload(
   sequelize: Sequelize,
@@ -48,14 +50,19 @@ export async function createMappedPayload(
         service: "validation"
       })
     });
+    logger.debug(`Looking up facility ${facility['facility-code']}.`);
+
     const facilityData = await mhfrClient.findFacilityByCode(facility['facility-code']);
 
     if (facilityData && getDHIS2OUCode(facilityData)) {
+      logger.debug(`Found facility ${facility['facility-code']}.`);
       const organizationUnitCode = getDHIS2OUCode(facilityData);
       const facilityId = facilityData.facility_code;
       for (const facilityValue of facility.values) {
+        logger.debug(`Looking up product ${facilityValue['product-code']}`);
         const productData = await productMasterClient.findProductByCode(facilityValue['product-code'], message.clientId);
-        if (productData && getProductDHIS2Code(productData))  {
+        if (productData && getProductDHIS2Code(productData)) {
+          logger.debug(`Found product ${facilityValue['product-code']}`);
           const dataElementCode = getProductDHIS2Code(productData);
           const mappedPayload = {
             dataElementCode,
@@ -68,17 +75,15 @@ export async function createMappedPayload(
           };
           mappedPayloads.push(mappedPayload);
         } else {
+          logger.error(`Could not find product ${facilityValue['product-code']}'s data element code.`);
+          await pushToValidationFailures(`Failed to find dataElement for ${facilityValue["product-code"]}`);
           validationError = true;
-          await pushToValidationFailures(
-            `Failed to find dataElement for ${facilityValue["product-code"]}`
-          );
         }
       }
     } else {
+      logger.error(`Could not find facility ${facility['facility-code']}'s organisation unit code.`);
+      await pushToValidationFailures(`Failed to find organizationUnitCode for ${facility["facility-code"]}`);
       validationError = true;
-      await pushToValidationFailures(
-        `Failed to find organizationUnitCode for ${facility["facility-code"]}`
-      );
     }
   }
   if (validationFailures.length > 0) {
