b05487403d219101307e8e175ebfed249c690208
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-30 09:17:09 +0200

    feat(Logging): add winston logger

---
+++ src/modules/logging/winston.logger.ts
@@ -0,0 +1,23 @@
+import { createLogger, format, transports as Transports } from 'winston';
+import { hostname } from 'os';
+
+const hostMeta = format((info, _opts) => Object.assign({}, info, { context: { host: hostname() } }));
+const _defaultFormat = format.combine(format.json(), format.timestamp(), hostMeta());
+
+const _winstonLogger = createLogger({
+    level: 'info',
+    defaultMeta: { service: 'ADX Validator' },
+    format: _defaultFormat,
+    transports: [
+        new Transports.File({ filename: 'logs/error.log', level: 'error', format: _defaultFormat }),
+        new Transports.File({ filename: 'logs/combined.log', format: _defaultFormat })
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    _winstonLogger.add(new Transports.Console({
+        format: _defaultFormat,
+    }));
+}
+
+export const LOGGER = _winstonLogger;
