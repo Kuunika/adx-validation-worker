83025e005276a1de8a4c29f75050f6298d67e69e
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-30 08:28:40 +0200

    fix(Helpers): account for null mappings

--- src/modules/facilities/helpers.ts
+++ src/modules/facilities/helpers.ts
@@ -2,7 +2,9 @@ import { SystemNames } from "../../common/constants";
 import { Facility } from "./facility.interface";
 
 export function getDHIS2OUCode(facility: Facility): string {
-    const code = facility.facility_code_mapping.find(val => val.system === SystemNames.DHIS2);
-    if (code) return code.code;
+    if (facility.facility_code_mapping) {
+        const code = facility.facility_code_mapping.find(val => val.system === SystemNames.DHIS2);
+        if (code) return code.code;
+    }
     return '';
 }
