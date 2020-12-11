c3528177df54b8deb4fbd57305c5d51c9048c780
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-12-11 10:24:56 +0200

    refactor(MHFRClient): add client dependent route
    - filtered by client if the client is DHAMIS or OpenLMIS
    - DHAMIS and OpenLMIS use their own facility codes

--- src/modules/facilities/master-health-facility.client.ts
+++ src/modules/facilities/master-health-facility.client.ts
@@ -2,12 +2,13 @@ import Axios, { AxiosError } from 'axios';
 import { Wrapper } from "../config/wrapper";
 import { LOGGER } from '../logging/winston.logger';
 import { Facility } from "./facility.interface";
+import { RegexMods, SystemNames } from '../../common/constants';
 import { MasterHealthFacilityClientGatewayException } from './master-health-facility-client.exception';
 
 export class MasterHealthFacilityClient {
-    async findFacilityByCode(code: string): Promise<Facility|null> {
+    async findFacilityByCode(code: string, system: string): Promise<Facility|null> {
         const host = Wrapper.get<string>('AVW_MHFR_URL');
-        const facilityPath = `${host}/Facilities/findByCode/${code}`;
+        const facilityPath = `${host}${resourcePath(code, system)}`;
         try {
             LOGGER.info(`Attempting to find facility ${facilityPath}.`);
             const facility = (await Axios.get(facilityPath)).data;
@@ -24,4 +25,12 @@ export class MasterHealthFacilityClient {
             // throw new MasterHealthFacilityClientGatewayException(`There was a problem talking to MHFR when fetching ${code}.`);
         }
     }
+}
+
+function resourcePath(code: string, system: string): string {
+    const pattern = new RegExp(system, RegexMods.CASE_INSENSITIVE);
+    if (pattern.test(SystemNames.DHAMIS) || pattern.test(SystemNames.OPENLMIS)) {
+        return `/Facilities/findBySystem?code=${code}&system=${system}`;
+    }
+    return `/Facilities/findByCode/${code}`;
 }
