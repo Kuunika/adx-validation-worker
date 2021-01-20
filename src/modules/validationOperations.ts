f2a01ae8d31f71ce90fdfcfe0740552b67834ddf
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-01-20 21:37:04 +0200

    refactor(validation: switch to caches

--- src/modules/validationOperations.ts
+++ src/modules/validationOperations.ts
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize";
-import { appendFileSync } from "fs";
 import {
   PostPayload,
   MigrationDataElement,
@@ -11,14 +10,11 @@ import {
 } from "../interfaces";
 import { persistValidationFailures } from ".";
 import { sendToLogQueue } from "./queueOperations";
-import { ProductMasterClient } from "./products/product-master.client";
-import { MasterHealthFacilityClient } from "./facilities/master-health-facility.client";
 import { LOGGER } from "./logging/winston.logger";
 import { getDHIS2OUCode } from "./facilities/helpers";
 import { getProductDHIS2Code } from "./products/helpers";
-
-const productMasterClient = new ProductMasterClient();
-const mhfrClient = new MasterHealthFacilityClient();
+import { createCache } from "./products/product-cache";
+import { createFacilitiesCache } from "./facilities/facilities.cache";
 
 export async function createMappedPayload(
   sequelize: Sequelize,
@@ -26,6 +22,10 @@ export async function createMappedPayload(
   migrationId: number,
   message: QueueMessage
 ): Promise<ValidationResult> {
+  const FACILITIES_CACHE = await createFacilitiesCache(message.clientId);
+  const PRODUCT_CACHE = await createCache();
+  const MAX_PACKET_SIZE = 1000;
+
   const { facilities } = payload;
   const fileName = `validation-failed-${Date.now()}.adx`;
   const validationFailures: ValidationFailure[] = [];
@@ -33,7 +33,6 @@ export async function createMappedPayload(
   let validationError = false;
 
   const pushToValidationFailures = async (reason: string) => {
-    appendFileSync(`logs/${fileName}`, reason);
     validationFailures.push({
       fileName,
       migrationId,
@@ -53,19 +52,16 @@ export async function createMappedPayload(
         service: "validation",
       }),
     });
-    LOGGER.info(`Looking up facility ${facility["facility-code"]}.`);
 
-    const facilityData = await mhfrClient.findFacilityByCode(
-      facility["facility-code"],
-      message.clientId
-    );
+    const facilityData = FACILITIES_CACHE[facility["facility-code"]];
 
     if (facilityData && getDHIS2OUCode(facilityData)) {
-      LOGGER.info(`Found facility ${facility["facility-code"]}.`);
       const organizationUnitCode = getDHIS2OUCode(facilityData);
       const facilityId = facilityData.facility_code;
+
       const promises = facility.values.map(p => validateProduct(p, organizationUnitCode, facilityId));
       await Promise.all(promises);
+
     } else {
       LOGGER.error(
         `Could not find facility ${facility["facility-code"]}'s organisation unit code.`
@@ -82,13 +78,9 @@ export async function createMappedPayload(
     ouCode: string,
     facility: string
   ): Promise<void> => {
-    LOGGER.info(`Looking up product ${product["product-code"]}`);
-    const productData = await productMasterClient.findProductByCode(
-      product["product-code"],
-      message.clientId
-    );
+    const productData = PRODUCT_CACHE[product["product-code"]];
+
     if (productData && getProductDHIS2Code(productData)) {
-      LOGGER.info(`Found product ${product["product-code"]}`);
       const dataElementCode = getProductDHIS2Code(productData);
       const mappedPayload = {
         dataElementCode,
@@ -111,11 +103,14 @@ export async function createMappedPayload(
     }
   };
 
-  const todo = facilities.map(f => validatePayload(f));
-  await Promise.all(todo);
+  for (const facility of facilities) {
+    await validatePayload(facility);
+  }
 
   if (validationFailures.length > 0) {
-    await persistValidationFailures(sequelize, validationFailures);
+    while (validationFailures.length) {
+      await persistValidationFailures(sequelize, validationFailures.splice(0, MAX_PACKET_SIZE));
+    }
   }
 
   return { migrationDataElements: mappedPayloads, validationError };
